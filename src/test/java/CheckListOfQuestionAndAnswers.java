331648336737802e236447dfe1da6a43c030c489
Elena Larkova <hamidulina.elena@mail.ru>
2023-02-12 00:30:57 +0700

    Исправленный итоговый вариант финального проекта 4-го спринта

--- src/test/java/CheckListOfQuestionAndAnswers.java
+++ src/test/java/CheckListOfQuestionAndAnswers.java
@@ -1,11 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
